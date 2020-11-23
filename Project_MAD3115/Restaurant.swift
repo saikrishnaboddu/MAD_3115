0ff8dc18894f37b131add973ac00b0f4fe2922b3
saikrishnaboddu <39978856+saikrishnaboddu@users.noreply.github.com>
2020-11-24 01:29:15 +0530

    Initial Design

---
+++ Project_MAD3115/Restaurant.swift
@@ -0,0 +1,15 @@
+//
+//  Restaurant.swift
+//  Project_MAD3115
+//
+//  Created by saikrishna B on 24/11/20.
+//
+
+import Foundation
+class Restaurant{
+    
+    var RestName=""
+    var RestLocation=""
+    
+    
+}
---
+++ Project_MAD3115/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_MAD3115
+//
+//  Created by saikrishna B on 23/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
